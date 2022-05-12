889af4c1ecd821d3bb79cda4299c2df9187538d9
Maximinodotpy <85794303+Maximinodotpy@users.noreply.github.com>
2022-05-12 13:49:19 +0200

    Update footer.php

--- assets/footer.php
+++ assets/footer.php
@@ -1,25 +1,28 @@
 <footer>
-    
 
-    <div class="grid grid-2 flex-between padding bg-dark-blue t-color-white">
-        <div>
-            <span class="t-color-white">a2-c AG </span><br>
-            <span class="t-color-white">Kammgarn West, 1. Stock</span><br>
-            <span class="t-color-white">Baumgartenstrasse 23</span><br>
-            <span class="t-color-white">8200 Schaffhausen</span><br>
-            <span class="t-color-white">052 533 66 60</span><br>
-            <span class="t-color-white">info@a2-c.ch</span><br>
-            <span class="t-color-white">www.a2-c.ch</span><br>
-        </div>
-        <div class="t-right">
-            <a class="reset-link link-light u-hover" href="index.php">Über uns </a><br>
-            <a class="reset-link link-light u-hover" href="wwm.php">Was wir machen </a><br>
-            <a class="reset-link link-light u-hover" href="lehre.php">Lehre </a><br>
-            <a class="reset-link link-light u-hover" href="kontakt">Kontakt </a><br>
-            <a class="reset-link link-light u-hover" href="disclaimer">Disclaimer & Datenschutz</a><br>
+    <div class="container-parent bg-dark-blue ">
+        <div class="container">
+
+            <div class="grid grid-2 flex-between padding-y t-color-white">
+                <div>
+                    <span class="t-color-white">a2-c AG </span><br>
+                    <span class="t-color-white">Kammgarn West, 1. Stock</span><br>
+                    <span class="t-color-white">Baumgartenstrasse 23</span><br>
+                    <span class="t-color-white">8200 Schaffhausen</span><br>
+                    <span class="t-color-white">052 533 66 60</span><br>
+                    <span class="t-color-white">info@a2-c.ch</span><br>
+                    <span class="t-color-white">www.a2-c.ch</span><br>
+                </div>
+                <div class="t-right">
+                    <a class="reset-link link-light u-hover" href="index.php">Über uns </a><br>
+                    <a class="reset-link link-light u-hover" href="wwm.php">Was wir machen </a><br>
+                    <a class="reset-link link-light u-hover" href="lehre.php">Lehre </a><br>
+                    <a class="reset-link link-light u-hover" href="kontakt">Kontakt </a><br>
+                    <a class="reset-link link-light u-hover" href="disclaimer">Disclaimer & Datenschutz</a><br>
+                </div>
+            </div>
+
         </div>
     </div>
 
-
 </footer>
-
